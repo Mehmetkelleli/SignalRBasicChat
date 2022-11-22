74d45c6ee42cfaed0795351c1fb979e5ba66cc12
MehmetKelleli <Kellelizx@gmail.com>
2022-11-22 14:51:02 +0300

    addedCreateUsersId

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -4,17 +4,22 @@ namespace signalrdersleri.Hubs
 {
     public class ChatHub:Hub
     {
+        private static List<string> Users = new List<string>();
         public async Task SendMessage(string message,string name)
         {
             await Clients.All.SendAsync("receiveMessage",message,name);
         }
         public override async Task OnConnectedAsync()
         {
+            Users.Add(Context.ConnectionId);
             await Clients.All.SendAsync("connectUser", Context.ConnectionId);
+            await Clients.All.SendAsync("Users", Users);
         }
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
+            Users.Remove(Context.ConnectionId);
             await Clients.All.SendAsync("disconnectUser", Context.ConnectionId);
+            await Clients.All.SendAsync("Users", Users);
         }
     }
 }
